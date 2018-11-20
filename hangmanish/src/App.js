f8ca24fec0857394f564757eb574df9bc0662852
Rob Olive <robert.aaron.olive@gmail.com>
2018-11-19 19:11:07 -0500

    file cleanup

--- hangmanish/src/App.js
+++ hangmanish/src/App.js
@@ -29,7 +29,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
             {currentArray.map((letter, i) => {
               return <Blank thisLetter={letter} key={i}/>
             })}
